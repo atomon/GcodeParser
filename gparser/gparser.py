3b5ea046d30db2b0e89dcfa586fda730df67d40f
atomon <robosystem_126@yahoo.co.jp>
2023-10-22 11:38:10 +0900

    feat: add save nc file

--- gparser/gparser.py
+++ gparser/gparser.py
@@ -5,36 +5,62 @@ import re
 class GcodeLine:
     def __init__(
             self,
-            command,
-            params,
-            comment
+            command: tuple[str, int],
+            params: dict,
+            comment: str
     ) -> None:
         self.command = command
         self.params = params
         self.comment = comment
 
-        print(command, params, comment)
+    def decode(
+            self,
+    ) -> str:
+        command_str = ''
+        params_str = ''
+        comment_str = ''
+
+        if self.command[0] != '#':
+            command_str = self.command[0] + str(self.command[1]).zfill(2)
+
+            params_str = ''
+            for key, val in self.params.items():
+                if key == 'F':
+                    params_str += f' {key}{val:.02f}'
+                else:
+                    params_str += f' {key}{val:.04f}'
+
+            if len(self.comment):
+                command_str = ' ' + self.comment
+
+        else:
+            comment_str = self.comment
+
+        return command_str + params_str + comment_str
 
 
-class GcodePaser:
+class GcodeParser:
     def __init__(self, gcodeline: list["GcodeLine"]) -> None:
         self.gline = gcodeline
+
+    def __len__(self) -> int:
+        return len(self.gline)
 
     @classmethod
     def from_flatcam(
             cls,
             file_path: Path
-    ) -> "GcodePaser":
+    ) -> "GcodeParser":
         with open(file_path, 'r') as f:
             texts = f.read()
 
-        return cls.from_bynary(texts)
+        return cls.from_flatcam_bynary(texts)
 
     @classmethod
-    def from_bynary(
+    def from_flatcam_bynary(
             cls,
             texts: str
-    ) -> "GcodePaser":
+    ) -> "GcodeParser":
         re_not_comennt = r'(?!\( *.+)'  # commnet flag: ()
         re_command = r'(G|M|T)(\d+)'
         re_param = r'(([ \t]*(?!G|M|g|m)[A-Z][-\d\.]*)*)'
@@ -43,7 +69,10 @@ class GcodePaser:
 
         gcodes: list = []
         for text in texts.split("\n"):
-            gcodes += re.findall(re_splitter, text)
+            if len(text):
+                gcodes += re.findall(re_splitter, text)
+            else:
+                gcodes += [('', '', '', '', '', '', '')]
 
         re_param_splitter = r'[ \t]*(?!G|M|g|m)([A-Z])([-\d\.]*)'
         gcodeline: list = []
@@ -57,7 +86,7 @@ class GcodePaser:
             if gcode[0]:
                 command = (gcode[0], int(gcode[1]))
                 params = {param[0]: float(param[1]) for param in param_list}
-                comment = gcode[5]
+                comment = gcode[4]
 
             else:
                 comment = gcode[5]
@@ -66,13 +95,29 @@ class GcodePaser:
 
         return cls(gcodeline)
 
+    def save(
+            self,
+            file_path: Path
+    ) -> None:
+        gcodes: str = ''
+        for gcode in self.gline:
+            gcodes += gcode.decode() + '\n'
+
+        with open(file_path, 'w') as f:
+            f.write(gcodes)
+
 
 if __name__ == "__main__":
     gcode = """G0 X46.2033 Y23.673 Z23.6739\n
                G10 X46.2033 Y23.673 Z23.6739\n
                (asdfbth12367G12 X12)\n
                G0 X46.2033 Y23.673 Z23.6739\n
-               G0 X46.2033 Y23.673 Z23.6739\n
+               G0 X46.2033 Y23.673 Z23.6739 (asdfrg)\n
                M30\n"""
+    test_path = Path('test.nc')
 
-    GcodePaser.from_bynary(gcode)
+    gs_bynary = GcodeParser.from_flatcam_bynary(gcode)
+    gs_file = GcodeParser.from_flatcam(test_path)
+
+    gs_bynary.save(Path('bynary.nc'))
+    gs_file.save(Path('file.nc'))
